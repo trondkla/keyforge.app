cd98d57d16b71abf439c78d14abe891f84f74e20
Trond Klakken <trond@klakken.com>
2018-11-13 10:35:41 +0100

    Remove direct links to keys

--- amber/amber.js
+++ amber/amber.js
@@ -4,32 +4,29 @@ function amberRender() {
   amountEl.innerHTML = amberAmount;
 }
 
-
 function increaseAmber() {
   amberAmount = amberAmount + 1;
   ga('send', { hitType: 'event', eventCategory: 'Amber', eventAction: 'increase', eventLabel: 'Usage' });
 
   if (amberAmount > 6) {
     amberAmount = 0;
-    forgeKey();
   }
   amberRender();
 }
 
-function decreaseAmber() {
-  amberAmount = amberAmount - 1;
+function decreaseAmber(amount) {
+  if (!amount) amount = 1;
+  amberAmount = amberAmount - amount;
   ga('send', { hitType: 'event', eventCategory: 'Amber', eventAction: 'decrease', eventLabel: 'Usage' });
 
   if (amberAmount <= 0 && keysForged > 0) {
     amberAmount = 6;
-    deforgeKey();
   }
   if (amberAmount <= 0 && keysForged === 0) {
     amberAmount = 0;
   }
   amberRender();
 }
-
 
 var plusButtonEl = document.getElementById("amber-plus-button");
 plusButtonEl.addEventListener("click", increaseAmber, false);
